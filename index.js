a0f022863b0080ecddfdf32231cb29c00734a560
ポール ウェッブ <netopwibby@thenetwork.email>
2018-04-24 14:55:44 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/* jshint undef: true, unused: true, esversion: 6, node: true */
+
+"use strict";
+
+
+
+/*
+  Take a personality quiz and then decide if you want your Navi to
+  have a similar personality to your's or not.
+*/
